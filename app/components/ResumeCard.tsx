c001fec9267a72ded700439a37d144c69c954879
Saurabh Tiwari <saurabh20tiwari04@gmail.com>
2025-12-23 23:04:53 +0530

    added the authentication

--- app/components/ResumeCard.tsx
+++ app/components/ResumeCard.tsx
@@ -2,20 +2,30 @@ import React from 'react'
 import { Link } from 'react-router'
 import ScoreCircle from './ScoreCircle'
 
-const ResumeCard = ({resume}: {resume: Resume}) => {
+const ResumeCard = ({resume: {id, companyName, jobTitle, feedback, imagePath}}: {resume: Resume}) => {
   return (
-   <Link to={`/resume/${resume.id}`} className='resume-card animate-in fade-in duration-1000'>
-    <div className='flex flex-col gap-2'>
+   <Link to={`/resume/${id}`} className='resume-card animate-in fade-in duration-1000'>
+<div className='resume-card-header'>
+   <div className='flex flex-col gap-2'>
         <h2 className='text-black font-bold wrap-break-word'>
-        {resume.companyName}
+        {companyName}
         </h2>
         <h3 className='text-lg wrap-break-word text-gray-500'>
-          {resume.jobTitle}
+          {jobTitle}
         </h3>
     </div>
-    { <div className='flex-shrink-0'>
+     <div className='flex-shrink-0'>
       <ScoreCircle score={feedback.overallScore} />
-    </div> }
+    </div> 
+</div>
+
+   <div className='gradient-border animate-in fade-in duration-1000'>
+      <div className='w-full h-full'>
+        <img src={imagePath} 
+        alt="resume"
+        className='w-full h-[350px] max-sm:h-[200px] object-cover object-top' />
+      </div>
+   </div>
    </Link>
   )
 }
